3ae444eafc6f945138eaa4714b367645af8ebf47
Luan Mateus <luanmateus2002@gmail.com>
2024-11-27 21:23:10 -0300

    usuario

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,18 +1,9 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Render,
-    Request,
-    Res,
-    UseFilters,
-    UseGuards,
-} from '@nestjs/common';
-import { Response } from 'express';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get()
+  @Get()
   @Render('users/index')
   index() {
     // Lógica para obter a lista de usuários, se necessário
