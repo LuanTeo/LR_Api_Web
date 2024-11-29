e2064cfdc790dc0c828e0a28bca546baf346bb5c
Luan Mateus <luanmateus2002@gmail.com>
2024-11-28 23:02:34 -0300

    usuario atualizado

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,14 +3,14 @@ import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('/users')
+  @Get()
   @Render('users/index')
   index() {
     // Lógica para obter a lista de usuários, se necessário
     return {};
   }
 
-  @Get('/users/create')
+  @Get('/create')
   @Render('users/formulario')
   create() {
     // Lógica para inicializar o formulário de cadastro, se necessário
